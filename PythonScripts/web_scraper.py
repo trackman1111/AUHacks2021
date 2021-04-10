923e32688d3d4da47c9e1790c70d14a65d627ae1
Jake Frassinelli <jakefrazz@gmail.com>
2021-04-10 18:45:44 -0500

    States and Mask orders scraped

--- PythonScripts/web_scraper.py
+++ PythonScripts/web_scraper.py
@@ -1,30 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-sectionDivs = []
-states = []
-info = []
-Orders = []
+def main ():
+    sectionDivs = []
+    states = []
+    info = []
+    orders = []
 
-URL = 'https://www.aarp.org/health/healthy-living/info-2020/states-mask-mandates-coronavirus/'
-page = requests.get(URL)
+    URL = 'https://www.aarp.org/health/healthy-living/info-2020/states-mask-mandates-coronavirus/'
+    page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html5lib')
-overDiv = soup.find_all("div", {"class": "everywhere-article-content parsys"})
+    soup = BeautifulSoup(page.content, 'html5lib')
+    overDiv = soup.find_all("div", {"class": "everywhere-article-content parsys"})
 
-for div in overDiv:
-    sectionDivs += div.find_all("div", {"class": "aarpe-text-image"})
+    for div in overDiv:
+        sectionDivs += div.find_all("div", {"class": "aarpe-text-image"})
 
-for subsec in sectionDivs:
-    for header in subsec.find_all("h4"):
-        states.append(header.text.rstrip("\n").rstrip("\xa0"))
-    for p in subsec.find_all("p"):
-        if "Statewide order" in p.text:
-            info += p
+    for subsec in sectionDivs:
+        for header in subsec.find_all("h4"):
+            states.append(header.text.rstrip("\n").rstrip("\xa0"))
+        for p in subsec.find_all("p"):
+            if "wide order" in p.text:
+                info += p
 
-for i in range(0,len(info),2):
-    print(info[i].text + info[i+1])
+    del states[-1]
+    info.insert(29, info[28].text.replace("Statewide order: ", ""))
+    info[28] = info[0]
 
-del states[-1]
+    for i in range(1,len(info),2):
+        orders.append(info[i].replace("\xa0","").replace(" ",""))
 
-print(Orders)
+    print(states)
+    print(orders)
+
+main()
