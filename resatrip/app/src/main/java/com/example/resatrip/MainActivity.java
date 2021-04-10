06793965af97091b132dd6474a12efe4f2b31395
Shane McNabb <shane.r.mcnabb@gmail.com>
2021-04-10 18:47:04 -0500

    changed MainActivity

--- resatrip/app/src/main/java/com/example/resatrip/MainActivity.java
+++ resatrip/app/src/main/java/com/example/resatrip/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.resatrip;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "penis";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     //sends data into the next page
     public void sendData(View view)
     {
-
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
