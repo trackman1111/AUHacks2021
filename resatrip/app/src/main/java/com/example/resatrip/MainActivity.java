63afb086a2657d076b0f87c4a5355466fb6bc91d
Shane McNabb <shane.r.mcnabb@gmail.com>
2021-04-10 18:35:10 -0500

    Changed strings and MainActivity
    Also learned a few valuable lessons about gitkraken

--- resatrip/app/src/main/java/com/example/resatrip/MainActivity.java
+++ resatrip/app/src/main/java/com/example/resatrip/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.resatrip;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //Class: sendData|Author: Shane
+    //sends data into the next page
+    public void sendData(View view)
+    {
+
     }
 }
