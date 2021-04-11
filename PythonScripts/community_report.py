53e23a57aceec41c538ec1c0d8df27c49791b20f
Brandon Morrell <brandon@morrellworld.net>
2021-04-10 19:15:47 -0500

    Wrote Communities to csv

--- PythonScripts/community_report.py
+++ PythonScripts/community_report.py
@@ -6,10 +6,31 @@ import pandas
 xls = pandas.ExcelFile("CommunityProfile.xlsx")
 sheet1 = pandas.read_excel(xls, "Counties")
 
+
 def main():
     first_column = sheet1.iloc[:, [0, 5, 16, 19, 20, 36]]
     print(first_column)
 
 
+def get_counties():
+    return sheet1.iloc[:, 0]
 
 
+def get_state():
+    return sheet1.iloc[:, 5]
+
+
+def get_cases_per_100k():
+    return sheet1.iloc[:, 16]
+
+
+def case_increase():
+    return sheet1.iloc[:, 19]
+
+
+def death_increase():
+    return sheet1.iloc[:, 20]
+
+
+def infectivity_rate():
+    return sheet1.iloc[:, 36]
--- PythonScripts/main.py
+++ PythonScripts/main.py
@@ -4,9 +4,71 @@
 import csv
 import community_report
 
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'American Samoa': 'AS',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'District of Columbia': 'DC',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Guam': 'GU',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Northern Mariana Islands':'MP',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Puerto Rico': 'PR',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virgin Islands': 'VI',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY'
+}
+
 if __name__ == '__main__':
     mainFile = open('CountyData.csv', 'w')
     csv_writer = csv.writer(mainFile)
     csv_writer.writerow(["County", "State", "Mask Mandate", "Case per 100k", "Case Increase", "Death Increase",
                          "Positivity Rate"])
-    community_report.main()
+
+    counties = community_report.get_counties()
+    for i in range(1, 3273):
+        csv_writer.writerow([counties[i], "", "", "", "", "", ""])
