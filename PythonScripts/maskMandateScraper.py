b3eb19a5bc881780da35f45ffda5c98ed2ee0ffd
Jake Frassinelli <jakefrazz@gmail.com>
2021-04-10 19:01:47 -0500

    Dictionary return
    Method now returns a dictionary of the state names coupled with their mask mandate status.

--- PythonScripts/maskMandateScraper.py
+++ PythonScripts/maskMandateScraper.py
@@ -1,22 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-def main ():
-    sectionDivs = []
-    states = []
+
+def get_mask_orders():
+    section_divs = []
     info = []
+    states = []
     orders = []
 
     URL = 'https://www.aarp.org/health/healthy-living/info-2020/states-mask-mandates-coronavirus/'
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, 'html5lib')
-    overDiv = soup.find_all("div", {"class": "everywhere-article-content parsys"})
+    over_div = soup.find_all("div", {"class": "everywhere-article-content parsys"})
 
-    for div in overDiv:
-        sectionDivs += div.find_all("div", {"class": "aarpe-text-image"})
+    for div in over_div:
+        section_divs += div.find_all("div", {"class": "aarpe-text-image"})
 
-    for subsec in sectionDivs:
+    for subsec in section_divs:
         for header in subsec.find_all("h4"):
             states.append(header.text.rstrip("\n").rstrip("\xa0"))
         for p in subsec.find_all("p"):
@@ -27,10 +28,11 @@ def main ():
     info.insert(29, info[28].text.replace("Statewide order: ", ""))
     info[28] = info[0]
 
-    for i in range(1,len(info),2):
-        orders.append(info[i].replace("\xa0","").replace(" ",""))
+    for i in range(1, len(info), 2):
+        orders.append(info[i].replace("\xa0", "").replace(" ", ""))
 
-    print(states)
-    print(orders)
+    return dict(zip(states,orders))
 
-main()
+get_mask_orders()
+
+
